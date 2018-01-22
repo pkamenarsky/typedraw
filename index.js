4d7afdb33a083a9e4bcfe9c942586507d8d0f763
Philip Kamenarsky <p.kamenarsky@gmail.com>
2018-01-22 02:02:57 +0100

    Types, adjustments

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const canvas = require('canvas');
 const fs = require('fs');
 
+function intersperse(a, x) {
+  return [].concat(...a.map(e => [x, e])).slice(1);
+}
+
+function splitConcat(words, str) {
+  return [].concat.apply([], words.map((w) => intersperse(w.split(str), str)));
+}
+
 function draw(file, typemap, text) {
   const cvs = new canvas(1280, 480);
   const ctx = cvs.getContext('2d');
@@ -9,78 +17,106 @@ function draw(file, typemap, text) {
 
   ctx._setFont('500', 'normal', 18, 'px', 'Fira Code');
 
-  const x = 18;
-  const y = 18;
+  const x = 20;
+  const y = 20;
 
   const spacing = 10.8;
   const leading = 36;
 
-  const offset_y = -18;
-  const offset_x = -2;
+  const offset_y = -19;
+  const offset_x = 3;
   const size = 27;
 
   lines.forEach((line, line_index) => {
     // ctx.fillText(line, x, y + line_index * leading);
 
-    let char_index = 0;
-    line.split(' ').forEach((word) => {
-      if (typemap[word] !== undefined) {
-        typemap[word].forEach(([nx, ny, nw, nh, color]) => {
+    let adjust = 0;
+    Object.entries(typemap).forEach(([k, v]) => {
+      const re = new RegExp('\\b' + k + '\\b', 'g');
+
+      while ((match = re.exec(line)) != null) {
+        v.forEach(([type, color, nx, ny, nw, nh ]) => {
           ctx.fillStyle = color;
-          ctx.fillRect(
-            nx * size + x + char_index * spacing + offset_x,
-            ny * size + y + line_index * leading + offset_y,
-            nw * size,
-            nh * size
-          );
+
+          switch (type) {
+          case 'rect':
+            ctx.fillRect(
+              nx * size + x + match.index * spacing - ((3 - k.length) / 2 * spacing) + offset_x,
+              ny * size + y + line_index * leading + offset_y,
+              nw * size,
+              nh * size
+            );
+            break;
+
+          case 'circle':
+            ctx.beginPath();
+            r = nw;
+            ctx.arc(
+              nx * k.length * spacing + x + match.index * spacing,
+              ny * size + y + line_index * leading - leading / 2 - 1,
+              r * size,
+              0,
+              2 * Math.PI
+            );
+            ctx.fill();
+            break;
+          }
         });
-
-        char_index += 3;
       }
-      else {
-        ctx.fillStyle = '#333';
-        ctx.fillText(word, x + char_index * spacing, y + line_index * leading);
 
-        // word.split('').forEach((c) => {
-        //   ctx.fillText(c, x + char_index * spacing, y + line_index * leading);
-        //   char_index++;
-        // });
-
-        char_index += word.length + 1;
-      }
+      line = line.replace(re, ' '.repeat(k.length));
     });
+
+    ctx.fillStyle = '#333';
+    ctx.fillText(line, x, y + line_index * leading);
   });
 
   fs.writeFileSync(file, cvs.toBuffer());
 }
 
 const typemap = {
-  'st': [
-    [ 0, 0, 1, 1, '#333' ],
-    [ 0.25, 0.25, 0.5, 0.5, '#D4145A' ]
+  'st1': [
+    [ 'rect', '#333', 0, 0, 1, 1 ],
+    [ 'rect', '#D4145A', 0.25, 0.25, 0.5, 0.5 ]
   ],
-  'stt': [
-    [ 0.25, 0.25, 0.5, 0.5, '#D4145A' ]
+  's': [
+    [ 'rect', '#D4145A', 0.25, 0.25, 0.5, 0.5 ]
   ],
   'k': [
-    [ 0.25, 0.25, 0.5, 0.5, '#29ABE2' ]
+    [ 'rect', '#29ABE2', 0.25, 0.25, 0.5, 0.5 ]
   ],
   'v': [
-    [ 0.25, 0.25, 0.5, 0.5, '#F7931E' ]
+    [ 'rect', '#F7931E', 0.25, 0.25, 0.5, 0.5 ]
+  ],
+};
+
+const typemap2 = {
+  'st': [
+    [ 'circle', '#333', 0.5, 0.5, 0.5 ],
+    [ 'circle', '#D4145A', 0.5, 0.5, 0.25 ]
+  ],
+  's': [
+    [ 'circle', '#D4145A', 0.5, 0.5, 0.25 ]
+  ],
+  'k': [
+    [ 'circle', '#29ABE2', 0.5, 0.5, 0.25 ]
+  ],
+  'v': [
+    [ 'circle', '#F7931E', 0.5, 0.5, 0.25 ]
   ]
 };
 
 const text =
-      'foreach :: ( k × v -> k × v -> Ordering ) \n' +
-      '           ( v -> Boolean ) \n' +
-      '        -> Lens st stt                             | stt ~ Map k v\n' +
-      '        -> ( Lens st v -> ( st -> st ) -> Component st )\n' +
+      'foreach :: (k × v -> k × v -> Ordering)\n' +
+      '        -> (v -> Boolean)\n' +
+      '        -> Lens st s                              -- | s ~ Map k v\n' +
+      '        -> (Lens st v -> (st -> st) -> Component st)\n' +
       '        -> Component st\n';
 
 const zoom =
-      'zoom :: Lens st stt -> Component stt -> Component st';
+      'zoom :: Lens st s -> Component s -> Component st';
 
 const zoomUn =
-      'zoomUn :: Lens st stt -> (((Component st -> Component stt ) -> Component stt ) -> Component st';
+      'zoomUn :: Lens st s -> (((Component u -> Component s) -> Component s) -> Component st';
 
-draw('out.png', typemap, text);
+draw('out.png', typemap2, zoomUn);
